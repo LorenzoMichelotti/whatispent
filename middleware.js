b73966893740422387f4a39a878f0d5c663e4927
lomichelotti <michelotti2k@gmail.com>
2023-09-15 03:33:37 -0300

    fix: middleware

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+  console.log("middleware run");
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
 
