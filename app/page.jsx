4679eb3e758c1c40dea3cef6e20811321c67735b
lomichelotti <michelotti2k@gmail.com>
2023-09-30 14:01:46 -0300

    style(loginscreen): mobile breakpoints

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Title from "@/components/title";
 
 export default function Home() {
   return (
-    <div className="w-2/3 sm:w-1/2 md:w-1/3 fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+    <div className="w-[75vw] sm:w-2/3 lg:w-1/2 xl:w-1/3 fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
       <Title></Title>
       <LoginForm></LoginForm>
     </div>
